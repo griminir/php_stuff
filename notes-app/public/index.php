cf025eafc6bd2ca77c3ddc7654e59915ff71de77
griminir <viktor.elliot.king@gmail.com>
2025-03-01 14:57:00 +0100

    reinitialized the composer to be in the right directory

--- notes-app/public/index.php
+++ notes-app/public/index.php
@@ -12,12 +12,12 @@ define('BASE_PATH', str_replace("\\", "/", __DIR__).'/../');
 
 require BASE_PATH.'Core/functions.php';
 
-spl_autoload_register(function ($class) {
-
-    $class = str_replace('\\', '/', $class);
-
-    require base_path("{$class}.php");
-});
+//spl_autoload_register(function ($class) {
+//
+//    $class = str_replace('\\', '/', $class);
+//
+//    require base_path("{$class}.php");
+//});
 
 require base_path('bootstrap.php');
 
